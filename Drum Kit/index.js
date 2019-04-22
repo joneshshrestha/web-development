da97fc0e356e17fbc2615a43acf03e2ea3631cc6
Jonesh Shrestha <contact@joneshshrestha.com>
2019-04-22 17:24:23 +0545

    learning

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,43 +1,54 @@
 for(let i=0; i<document.querySelectorAll('.drum').length; i++) {
   document.querySelectorAll('button')[i].addEventListener('click', function() {
-    switch (this.innerHTML) {
-      case "w":
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
+     let buttonInnerHTML = this.innerHTML
+     makeSound(buttonInnerHTML)
+  })
+}
+
+document.addEventListener('keypress', function() {
+  let keyPressed = event.key
+  makeSound(keyPressed)
+})
+
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+      case "a":
+        let kickbass = new Audio("sounds/kick-bass.mp3");
+        kickbass.play();
         break;
 
-        case "a":
-          let kickbass = new Audio("sounds/kick-bass.mp3");
-          kickbass.play();
-          break;
+      case "s":
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
 
-        case "s":
-          let snare = new Audio("sounds/snare.mp3");
-          snare.play();
-          break;
+      case "d":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
 
-        case "d":
-          let tom1 = new Audio("sounds/tom-1.mp3");
-          tom1.play();
-          break;
+      case "j":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
 
-        case "j":
-          let tom2 = new Audio("sounds/tom-2.mp3");
-          tom2.play();
-          break;
+      case "k":
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
 
-        case "k":
-          let tom3 = new Audio("sounds/tom-3.mp3");
-          tom3.play();
-          break;
+      case "l":
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
 
-        case "l":
-          let tom4 = new Audio("sounds/tom-4.mp3");
-          tom4.play();
-          break;
-
-        default:
-        console.log()
-    }
-  })
+      default:
+      console.log()
+  }
 }
