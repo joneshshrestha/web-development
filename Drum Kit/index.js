03ec644517e8e947a6160567b690636a2ef8025c
Jonesh Shrestha <contact@joneshshrestha.com>
2019-04-22 13:00:46 +0545

    learning

---
+++ Drum Kit/index.js
@@ -0,0 +1,43 @@
+for(let i=0; i<document.querySelectorAll('.drum').length; i++) {
+  document.querySelectorAll('button')[i].addEventListener('click', function() {
+    switch (this.innerHTML) {
+      case "w":
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+        case "a":
+          let kickbass = new Audio("sounds/kick-bass.mp3");
+          kickbass.play();
+          break;
+
+        case "s":
+          let snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+
+        case "d":
+          let tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+
+        case "j":
+          let tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+
+        case "k":
+          let tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+
+        case "l":
+          let tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+
+        default:
+        console.log()
+    }
+  })
+}
