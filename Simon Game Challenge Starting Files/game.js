c5379e8281d2f2b93a97404cb71beb182b10fef7
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-04 18:11:57 +0545

    update

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -6,25 +6,25 @@ let userClickedPattern = []
 let level = 0
 
 function nextSequence() {
+    userClickedPattern = []
+    let randomNumber = Math.floor(Math.random() * 4)
+    let randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour);
 
-  let randomNumber = Math.floor(Math.random() * 4)
-  let randomChosenColour = buttonColours[randomNumber]
-  gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+    playSound(randomChosenColour)
 
-  playSound(randomChosenColour)
-
-  level += 1
-  $("h1").text("Level " + level)
+    level++
+    $("h1").text("Level " + level)
 }
 
-$(".btn").click(function() {
+$(".btn").click(function () {
     let userChoosenColor = event.path[0].id
     userClickedPattern.push(userChoosenColor)
-    console.log(userClickedPattern)
     animatePress(userChoosenColor)
     playSound(userChoosenColor)
+    checkAnswer(userClickedPattern.length)
 })
 
 function playSound(name) {
@@ -34,30 +34,41 @@ function playSound(name) {
 
 function animatePress(currentColor) {
     $("#" + currentColor).addClass("pressed")
-    setTimeout(function() {
+    setTimeout(function () {
         $("#" + currentColor).removeClass("pressed")
     }, 100)
 }
 
 // Detect keyboard key press only for the first time
-$(document).one("keypress", function() {
+$(document).one("keypress", function () {
     nextSequence()
 })
 
 function checkAnswer(currentLevel) {
-    if (compareArray(currentLevel) == 1) {
-        setTimeout(nextSequence(), 1000)
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
     } else {
-        $("h1").text("You Loose!")
+        console.log("wrong");
     }
 }
 
-function compareArray(index) {
-    for(let i=0; i < index; i++) {
-        if(userClickedPattern[i] == gamePattern[i]) {
-            return 1
-        } else {
-            return 0
-        }
-    }
-}
+// function compareArray(index) {
+//     for (let i = 0; i < index; i++) {
+//         if (userClickedPattern[i] === gamePattern[i]) {
+//             return 1
+//         } else break;
+//     }
+// }
+
+// function compareArray() {
+//     if (JSON.stringify(userClickedPattern) === JSON.stringify(gamePattern)) {
+//         return 1
+//     } else {
+//         return 0
+//     }
+// }
