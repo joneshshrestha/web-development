65ce652226c753be89329304644b8ff7ad300ac7
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-04 16:56:07 +0545

    update

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,28 +1,63 @@
+let buttonColours = ["red", "blue", "green", "yellow"]
+
 let gamePattern = []
-let buttonColors = ["red", "blue", "green", "yellow"]
+let userClickedPattern = []
+
+let level = 0
+
 function nextSequence() {
-    return Math.floor(Math.random()*4)
+
+  let randomNumber = Math.floor(Math.random() * 4)
+  let randomChosenColour = buttonColours[randomNumber]
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+
+  playSound(randomChosenColour)
+
+  level += 1
+  $("h1").text("Level " + level)
 }
-let randomNumber = nextSequence()
-let randomChoosenColor = buttonColors[randomNumber]
-gamePattern.push(randomChoosenColor)
 
-$("div#" + randomChoosenColor).fadeOut(250).fadeIn(250)
+$(".btn").click(function() {
+    let userChoosenColor = event.path[0].id
+    userClickedPattern.push(userChoosenColor)
+    console.log(userClickedPattern)
+    animatePress(userChoosenColor)
+    playSound(userChoosenColor)
+})
 
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3")
+    audio.play()
+}
 
-// $("div#green").click(function() {
-//     $("audio#green")[0].play()
-// })
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed")
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed")
+    }, 100)
+}
 
-// $("div#red").click(function() {
-//     $("audio#red")[0].play()
-// })
+// Detect keyboard key press only for the first time
+$(document).one("keypress", function() {
+    nextSequence()
+})
 
-// $("div#yellow").click(function() {
-//     $("audio#yellow")[0].play()
-// })
+function checkAnswer(currentLevel) {
+    if (compareArray(currentLevel) == 1) {
+        setTimeout(nextSequence(), 1000)
+    } else {
+        $("h1").text("You Loose!")
+    }
+}
 
-// $("div#blue").click(function() {
-//     $("audio#blue")[0].play()
-// })
-
+function compareArray(index) {
+    for(let i=0; i < index; i++) {
+        if(userClickedPattern[i] == gamePattern[i]) {
+            return 1
+        } else {
+            return 0
+        }
+    }
+}
