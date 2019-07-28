dac5480ece522ab208112afdae5309876711a5f0
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-28 16:56:47 +0545

    update

--- node-js/index.js
+++ node-js/index.js
@@ -1,4 +1,8 @@
 let superheroes = require("superheroes")
+let supervillains = require("supervillains")
 
 let mySuperheroName = superheroes.random()
- console.log(mySuperheroName)
+console.log(mySuperheroName)
+
+let mySuperVillains = supervillains.random()
+console.log(mySuperVillains)
