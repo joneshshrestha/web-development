b1ec3fe5afc7ae43d3149ea97c486cd31a5c656d
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-29 10:13:28 +0545

    update

---
+++ Calculator/calculator.js
@@ -0,0 +1,10 @@
+let express = require("express")
+let app = express()
+
+app.get("/", function(req, res) {
+    res.send("Hello World")
+})
+
+app.listen(3000, function(){
+    console.log("Listening at Port 3000")
+})
