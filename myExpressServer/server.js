ee5b5804931ac19eaacacc1c335212dc8d0e7524
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-29 09:45:50 +0545

    express server

--- myExpressServer/server.js
+++ myExpressServer/server.js
@@ -2,8 +2,8 @@ let express = require("express")
 
 let app = express()
 
-app.get("/", function(request, response) {
-    response.send("Hello")
+app.get("/", function(req, res) {
+    response.send("<h1>Hello</h1>")
 })
 app.listen(3000, function(){
     console.log("Server started at port 3000")
