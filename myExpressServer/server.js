8f69629b330fe92f0013a28e44885704e7b3201a
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-29 09:52:02 +0545

    express server

--- myExpressServer/server.js
+++ myExpressServer/server.js
@@ -3,8 +3,17 @@ let express = require("express")
 let app = express()
 
 app.get("/", function(req, res) {
-    response.send("<h1>Hello</h1>")
+    res.send("<h1>Hello</h1>")
 })
+
+app.get("/contact", function(req, res){
+    res.send("<h3>contact@joneshshrestha.com</h3>")
+})
+
+app.get("/about", function(req, res){
+    res.send("My name is Jonesh Shrestha. Undergraduate at Kathmandu University")
+})
+
 app.listen(3000, function(){
     console.log("Server started at port 3000")
 })
