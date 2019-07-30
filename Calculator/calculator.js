6ec11457181e63e9e6439d1546d8c871cda6ab68
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-30 09:20:06 +0545

    update

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,8 +1,9 @@
 let express = require("express")
+
 let app = express()
 
 app.get("/", function(req, res) {
-    res.send("Hello World")
+    res.send("Hello World!")
 })
 
 app.listen(3000, function(){
