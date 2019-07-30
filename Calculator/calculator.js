85e7469620bd2bb4412d110544c5b46ce1c31dcf
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-30 09:54:48 +0545

    body-parser dependency

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
+app.post("/", function(req, res){
+    res.send("Thanks for posting")
+})
+
 app.listen(3000, function(){
     console.log("Listening at Port 3000")
 })
