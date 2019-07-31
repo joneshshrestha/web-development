6539d258e0427f20a257ffcd80d510e9e61ea0d0
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-31 20:21:08 +0545

    body-parser update

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,12 +1,16 @@
 let express = require("express")
+let bodyParser = require("body-parser")
 
 let app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
 app.post("/", function(req, res){
+
+    console.log(req.body)
     res.send("Thanks for posting")
 })
 
