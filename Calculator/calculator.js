9164db925dacae67f9acabca9185ead62974e9bd
Jonesh Shrestha <contact@joneshshrestha.com>
2019-08-01 13:07:34 +0545

    bmi calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -8,6 +8,18 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", function(req, res){
+    let weight = parseFloat(req.body.weight)
+    let height = parseFloat(req.body.height)
+
+    let bmi = weight / (height * height)
+    res.send("Your BMI is " + bmi)
+})
+
 app.post("/", function(req, res){
 
     console.log(req.body)
