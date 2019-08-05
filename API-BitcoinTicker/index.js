2f6828bbe738feeaf685b07310cf1d2d676ece55
Jonesh Shrestha <contact@joneshshrestha.com>
2019-08-05 17:54:18 +0545

    add express

--- API-BitcoinTicker/index.js
+++ API-BitcoinTicker/index.js
@@ -2,6 +2,7 @@
 
 let express = require("express")
 let bodyParser = require("body-parser")
+let request = require("request")
 
 let app = express()
 
@@ -9,6 +10,15 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res){
+    // console.log(req.body.crypto)
+
+    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
+        console.log(body)
+    })
+
 })
 
 app.listen(3000, function(){
