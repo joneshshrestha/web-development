a93ba65b95a8bb555babdbe9911112f0886bb67e
Jonesh Shrestha <contact@joneshshrestha.com>
2019-08-07 15:22:06 +0545

    add conversion

--- API-BitcoinTicker/index.js
+++ API-BitcoinTicker/index.js
@@ -16,24 +16,28 @@ app.post("/", function(req, res){
     // console.log(req.body.crypto)
     let crypto = req.body.crypto
     let fiat = req.body.fiat
-
-    let baseURL = "https://apiv2.bitcoinaverage.com/convert/global"
-    let finalURL = baseURL + crypto + fiat
+    let amount = req.body.amount
 
     let options = {
-        url: baseURL,
+        url: "https://apiv2.bitcoinaverage.com/convert/global",
+        method: "GET",
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount
+        }
 
     }
 
     request(options, function(error, response, body){
         let data = JSON.parse(body)
-        let price = data.last
+        let price = data.price
         console.log(price)
 
-        let currentDate = data.display_timestamp
+        let currentDate = data.time
 
         res.write("<p> The current date is " + currentDate + "</p>")
-        res.write("<h1> The current price of "+ crypto + " is " + price + " " + fiat + " <h1>")
+        res.write("<h1>"+ amount + " " + crypto + " is currently worth " + price + " " + fiat + " <h1>")
         res.send()
     })
 
