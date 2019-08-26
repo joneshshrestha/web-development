42e6f510147f83f6aa0cb7a8288cea39a87e4aeb
joneshshrestha <contact@joneshshrestha.com>
2019-08-26 18:36:35 +0545

    add css

--- todoList-v1/app.js
+++ todoList-v1/app.js
@@ -10,6 +10,7 @@ let items = ["Buy Food", "Cook Food", "Eat Food"]
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
     let today = new Date()
