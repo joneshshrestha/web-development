9e3a2ef3fde37e93cc34d0c052b7654312a6866e
joneshshrestha <contact@joneshshrestha.com>
2019-08-29 18:48:38 +0545

    update date.js

--- todoList-v1/date.js
+++ todoList-v1/date.js
@@ -1,8 +1,12 @@
-let today = new Date()
-let options = {
+function getDate() {
+    
+    let today = new Date()
+    let options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long'
-}
+    }
 
-let day = today.toLocaleDateString("en-US", options)
+    let day = today.toLocaleDateString("en-US", options)
+    return day
+}
