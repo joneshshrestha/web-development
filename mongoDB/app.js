bcf6987548573670ad7f65bc40ed2b54faf20bd2
joneshshrestha <contact@joneshshrestha.com>
2019-09-20 14:33:01 +0545

    update

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -20,7 +20,9 @@ client.connect(function(err) {
 
   const db = client.db(dbName);
 
-  client.close();
+  findDocuments(db, function() {
+    client.close();
+  });
 });
 
 const insertDocuments = function(db, callback) {
@@ -53,4 +55,16 @@ const insertDocuments = function(db, callback) {
       callback(result);
     }
   );
+};
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection("fruits");
+  // Find some documents
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits);
+    callback(fruits);
+  });
 };
