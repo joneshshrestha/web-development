d96fd2f7d01b861251aaa9220c571f3f7a69b0d1
joneshshrestha <contact@joneshshrestha.com>
2019-09-25 20:15:18 +0545

    update

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -25,7 +25,19 @@ const fruit = new Fruit({
   review: "Peaches are so yummy"
 });
 
-fruit.save();
+// fruit.save();
+
+Fruit.updateOne(
+  { _id: "5d8b761c8d04ac0b0057a3f0" },
+  { name: "Peach" },
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully updated the database.");
+    }
+  }
+);
 
 const personSchema = new mongoose.Schema({
   name: String,
